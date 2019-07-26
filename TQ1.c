a6d56f7255908977b8a6adb5d2f97eea4c1d9fe9
LutherAX2 <luther.jnbaptiste@my.uwi.edu>
2019-07-26 18:00:10 -0400

    Update TQ1.c

--- TQ1.c
+++ TQ1.c
@@ -24,7 +24,7 @@ void main (void)
     TRISB = 0x00;
     ram_init();
     sectorErase(0x34);
-	writeDATA(0x34,0x466,0x79);
+	writeDATA(0x34,0x466,0xF3);
     readDATA(0x34,0x466);
     PORTB = RXbits.CHARBITS;
     while(1);
